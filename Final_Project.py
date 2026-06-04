de58c4846f387db1d145b67dbcdc7dbfd916df90
JudahRegen <jregen@ucsd.edu>
2026-06-04 18:34:15 -0500

    Committed changes

--- Final_Project.py
+++ Final_Project.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
 df = pd.read_csv('EEG_data.csv')
-print(df.head())
-print(df.shape)
+
+# debugging 
+summary_yes_no = 0
+if summary_yes_no == 1:
+    print(df.shape)
+    print(df.dtypes)
+    print(df.head())
+    print(df.describe())
+
+# create subject dataframes
+subject_df = df.groupby('subject_id')
+
+s1 = df[df['subject_id'] == 1]
